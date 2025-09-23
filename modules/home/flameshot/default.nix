35b6e4578a5642110f1a0119c268feae8fb37531
permees <somdp2907@gmail.com>
2025-09-24 01:48:57 +0700

    feat(flameshot): update conf

--- modules/home/flameshot/default.nix
+++ modules/home/flameshot/default.nix
@@ -1,7 +1,24 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   services.flameshot = {
     enable = true;
     package = (pkgs.flameshot.override { enableWlrSupport = true; });
+    settings = {
+      General = {
+        showStartupLaunchMessage = false;
+
+        # Auto save to this path
+        savePath = "${config.home.homeDirectory}/Pictures/screenshots";
+        savePathFixed = true;
+        saveAsFileExtension = ".jpg";
+        filenamePattern = "%F_%H-%M";
+        drawThickness = 1;
+        copyPathAfterSave = true;
+
+        # For wayland
+        useGrimAdapter = true;
+        disabledGrimWarning = true;
+      };
+    };
   };
 }
