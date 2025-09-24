e8acb92823c4d319379d2700de184def3e6da95c
permees <somdp2907@gmail.com>
2025-09-24 10:56:05 +0700

    feat(hypr): update conf autostart

--- modules/home/hyprland/settings/autostart.nix
+++ modules/home/hyprland/settings/autostart.nix
@@ -1,7 +1,6 @@
 {
   wayland.windowManager.hyprland.settings."exec-once" = [
     "rfkill unblock bluetooth"
-    "1password"
     "swww-daemon"
     "swww img ~/Pictures/wallpapers/horizontal.jpg"
     "waybar &"
@@ -10,7 +9,7 @@
     "[workspace 2 silent] zen"
     "[workspace 3 silent] teams-for-linux"
     "[workspace 3 silent] slack"
-    # "[workspace 3 silent] thunderbird"
-    # "[workspace 10 silent] spotify"
+    "[workspace 3 silent] thunderbird"
+    "[workspace 9 silent] 1password"
   ];
 }
